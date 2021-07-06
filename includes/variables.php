dbeef984cb18441bd4ef2da5d30dbee5ab00c3c3
James Mellor <mellor29j@gmail.com>
2021-07-06 21:42:12 +0200

    init

---
+++ includes/variables.php
@@ -0,0 +1,5 @@
+<?php
+
+defined("PAGE_TITLE") ? null : define("PAGE_TITLE", "James Mellor - Software Developer");
+
+?>
---
+++ subpages/landing.php
@@ -0,0 +1,24 @@
+<section class="landing">
+    <div class="landing-container">
+        <video autoplay muted loop class="landing-container-video">
+            <source src="img/bgvideo.mp4" type="video/mp4">
+        </video>
+        <div class="landing-container-overlay">
+            <div class="landing-container-overlay-terminal">
+                <div class="landing-container-overlay-terminal-topbar">
+                    <span class="landing-container-overlay-terminal-topbar-text">
+                        user@user: ~
+                    </span>
+                    <span>
+                        <span class="landing-container-overlay-terminal-topbar-controls">&minus;</span>
+                        <span class="landing-container-overlay-terminal-topbar-controls">&square;</span>
+                        <span class="landing-container-overlay-terminal-topbar-controls">&times;</span>
+                    </span>
+                </div>
+                <p class="landing-container-overlay-terminal-text">
+                    user@User: ~ James Mellor, Software Developer
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
