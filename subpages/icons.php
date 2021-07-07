208f46d5f58af76ffe02fa77edde3565ab2ed7ee
James Mellor <mellor29j@gmail.com>
2021-07-07 16:48:26 +0200

    Added icons to work on

---
+++ subpages/icons.php
@@ -0,0 +1,6 @@
+<nav class="navbar">
+    <img src="img/command-window.png" alt="Home" width="auto" height="60">
+    <img src="img/doubts-button.png" alt="About" width="auto" height="60">
+    <img src="img/folder(1).png" alt="Portfolio" width="auto" height="60">
+    <img src="img/letter.png" alt="Contact" width="auto" height="60">
+</nav>
--- subpages/landing.php
+++ subpages/landing.php
@@ -4,10 +4,13 @@
             <source src="img/bgvideo.mp4" type="video/mp4">
         </video>
         <div class="landing-container-overlay">
-            <?php require_once("terminal.php"); ?>
-            <?php require_once("about.php"); ?>
-            <?php require_once("contact.php"); ?>
-            <?php require_once("portfolio.php"); ?>
+            <?php
+                require_once("icons.php");
+                require_once("terminal.php");
+                require_once("about.php");
+                require_once("contact.php");
+                require_once("portfolio.php");
+            ?>
         </div>
     </div>
 </main>
