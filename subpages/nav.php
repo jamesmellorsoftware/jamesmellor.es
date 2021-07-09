97149502adcca3afba74fc10a0ee4fd7ce0e593b
James Mellor <mellor29j@gmail.com>
2021-07-09 19:06:14 +0200

    Changed nav layout

--- subpages/nav.php
+++ subpages/nav.php
@@ -1,22 +1,16 @@
-<nav class="navbar">
-    <span>
-        <img class="navbar-icon" id="home" src="img/command-window.png" alt="Home" width="auto" height="60">
-        Home
-    </span>
-    <span>
-        <img class="navbar-icon" id="about" src="img/doubts-button.png" alt="About" width="auto" height="60">
-        About
-    </span>
-    <span>
-        <img class="navbar-icon" id="portfolio" src="img/folder(1).png" alt="Portfolio" width="auto" height="60">
-        My Work
-    </span>
-    <span>
-        <img class="navbar-icon" id="contact" src="img/letter.png" alt="Contact" width="auto" height="60">
-        Contact
-    </span>
-    <span>
-        <img class="navbar-icon" id="settings" src="img/settings.png" alt="Settings" width="auto" height="60">
-        Settings
-    </span>
+<nav class="navbar navbar-left">
+    <img class="navbar-icon" id="home" src="img/command-window.png" alt="Home" width="auto" height="60">
+    Home
+    <img class="navbar-icon" id="about" src="img/doubts-button.png" alt="About" width="auto" height="60">
+    About
+    <img class="navbar-icon" id="portfolio" src="img/folder(1).png" alt="Portfolio" width="auto" height="60">
+    My Work
+</nav>
+<nav class="navbar navbar-right">
+    <img class="navbar-icon" id="contact" src="img/letter.png" alt="Contact" width="auto" height="60">
+    Contact
+    <img class="navbar-icon" id="settings" src="img/settings.png" alt="Settings" width="auto" height="60">
+    Settings
+    <img class="navbar-icon" id="help" src="img/info.png" alt="Settings" width="auto" height="60">
+    Website Help
 </nav>
