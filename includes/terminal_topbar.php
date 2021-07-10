dc97dd4e949b06e4928e5c573d1418e9ed147b5b
James Mellor <mellor29j@gmail.com>
2021-07-10 18:58:23 +0200

    Topbar buttons look better

---
+++ includes/terminal_topbar.php
@@ -0,0 +1,6 @@
+<span class="terminal-topbar-controls">
+    <span class="terminal-topbar-controls-control terminal-topbar-contact-controls-control minimise">&minus;</span>
+    <span class="terminal-topbar-controls-control terminal-topbar-contact-controls-control maximise">&square;</span>
+    <span class="terminal-topbar-controls-control terminal-topbar-contact-controls-control restore">R</span>
+    <span class="terminal-topbar-controls-control terminal-topbar-contact-controls-control close">&times;</span>
+</span>
--- subpages/contact.php
+++ subpages/contact.php
@@ -3,12 +3,7 @@
         <span class="terminal-topbar-text terminal-about-contact-text">
             jamesmellor.es/contact
         </span>
-        <span>
-            <span class="terminal-topbar-controls terminal-topbar-contact-controls minimise">&minus;</span>
-            <span class="terminal-topbar-controls terminal-topbar-contact-controls maximise">&square;</span>
-            <span class="terminal-topbar-controls terminal-topbar-contact-controls restore">R</span>
-            <span class="terminal-topbar-controls terminal-topbar-contact-controls close">&times;</span>
-        </span>
+        <?php require_once("includes/terminal_topbar.php"); ?>
     </div>
 
     <div class="terminal-content terminal-contact-content">
