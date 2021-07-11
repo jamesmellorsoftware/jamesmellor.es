1b69caecb938831bbf5debc85fb8e7ff1e3131c0
James Mellor <mellor29j@gmail.com>
2021-07-11 15:16:50 +0200

    Re-did grid layout for easier mobile view design

--- subpages/landing.php
+++ subpages/landing.php
@@ -5,8 +5,8 @@
         </video>
         <div class="landing-container-overlay">
             <?php
-                require_once("intro.php");
                 require_once("nav.php");
+                require_once("no_windows.php");
                 require_once("home.php");
                 require_once("about.php");
                 require_once("contact.php");
---
+++ subpages/no_windows.php
@@ -0,0 +1,4 @@
+<section class="no_windows">
+    <p>jamesmellor.es</p>
+    <p>Click an icon to continue.</p>
+</section>
