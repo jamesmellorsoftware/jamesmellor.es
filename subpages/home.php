c9cea265b6a6fd53955547343f0bd7cc58456621
James Mellor <mellor29j@gmail.com>
2021-07-20 15:08:07 +0200

    Started work on terminal

--- subpages/home.php
+++ subpages/home.php
@@ -8,20 +8,68 @@
         <?php require("includes/terminal_topbar.php"); ?>
     </div>
     <div class="terminal-content terminal-home-content">
-<pre>
-// ===========================================//
-// ===========================================//
 
-░░█ ▄▀█ █▀▄▀█ █▀▀ █▀   █▀▄▀█ █▀▀ █░░ █░░ █▀█ █▀█
-█▄█ █▀█ █░▀░█ ██▄ ▄█   █░▀░█ ██▄ █▄▄ █▄▄ █▄█ █▀▄
+        <div class="terminal-home-content-space">&nbsp;</div>
 
-|| SOFTWARE DEVELOPER 25.0 ||
+        <pre class="terminal-home-content-ascii">
+        // ===========================================//
+        // ===========================================//
 
-// ===========================================//
-// ===========================================//
-</pre>
-        <p class="terminal-text terminal-home-text">
-            user@User: ~ James Mellor, Software Developer
-        </p>
+        ░░█ ▄▀█ █▀▄▀█ █▀▀ █▀   █▀▄▀█ █▀▀ █░░ █░░ █▀█ █▀█
+        █▄█ █▀█ █░▀░█ ██▄ ▄█   █░▀░█ ██▄ █▄▄ █▄▄ █▄█ █▀▄
+
+        || SOFTWARE DEVELOPER ||
+
+        // ===========================================//
+        // ===========================================//
+        </pre>
+
+        <div class="terminal-home-content-operations">
+            <div class="terminal-home-content-operations-intro">
+                <p class="terminal-text terminal-home-text">[ Loading: "background"...</p>
+                <p class="terminal-text terminal-home-text">[ "background" loaded</p>
+                <p class="terminal-text terminal-home-text">[ Loading: "jamesmellor.es/terminal"...</p>
+                <p class="terminal-text terminal-home-text">[ "jamesmellor.es/terminal" loaded</p>
+                <p class="terminal-text terminal-home-text">[ Loading: "jamesmellor.es/about"...</p>
+                <p class="terminal-text terminal-home-text">[ "jamesmellor.es/about" loaded</p>
+                <p class="terminal-text terminal-home-text">[ Loading: "jamesmellor.es/portfolio"...</p>
+                <p class="terminal-text terminal-home-text">[ "jamesmellor.es/portfolio" loaded</p>
+                <p class="terminal-text terminal-home-text">[ Loading: "jamesmellor.es/contact"...</p>
+                <p class="terminal-text terminal-home-text">[ "jamesmellor.es/contact" loaded</p>
+                <p class="terminal-text terminal-home-text">[ Loading: "lights.js"...</p>
+                <p class="terminal-text terminal-home-text">[ "lights.js" loaded</p>
+                <p class="terminal-text terminal-home-text">[ Loading: "langselector.js"...</p>
+                <p class="terminal-text terminal-home-text">[ "langselector.js" loaded</p>
+                <p class="terminal-text terminal-home-text">[ langselector.js > setting language: "en" (English)</p>
+            </div>
+            <div class="terminal-home-content-operations-welcome">
+                <p class="terminal-text terminal-home-text">&nbsp;</p>
+                <p class="terminal-text terminal-home-text">Welcome to jamesmellor.es!</p>
+                <p class="terminal-text terminal-home-text">&nbsp;</p>
+                <p class="terminal-text terminal-home-text"><- Choose an icon to begin -></p>
+                <p class="terminal-text terminal-home-text">&nbsp;</p>
+                <p class="terminal-text terminal-home-text">
+                    If this is your first time visiting, why not check the <span>About section?</span>
+                </p>
+                <p class="terminal-text terminal-home-text">&nbsp;</p>
+                <p class="terminal-text terminal-home-text">
+                    Alternatively, for a list of terminal commands, type: commandlist
+                </p>
+                <p class="terminal-text terminal-home-text">&nbsp;</p>
+            </div>
+            <p class="terminal-text terminal-home-text">
+                [ user@User: ~ James Mellor, Software Developer
+            </p>
+        </div>
+
+        <div class="terminal-home-content-space">
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+        </div>
+
     </div>
 </section>
