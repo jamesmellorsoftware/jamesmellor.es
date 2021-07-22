272dbc6164cbe11d8d2faf22f128f4504d49de31
James Mellor <mellor29j@gmail.com>
2021-07-22 13:27:41 +0200

    Adaptations for mobile, further visual changes

--- subpages/no_windows.php
+++ subpages/no_windows.php
@@ -1,5 +1,13 @@
 <section class="no_windows">
-    <p><?php echo PAGE_URL; ?></p>
+<pre class="no_windows-ascii">
+░░█ ▄▀█ █▀▄▀█ █▀▀ █▀
+█▄█ █▀█ █░▀░█ ██▄ ▄█
+
+█▀▄▀█ █▀▀ █░░ █░░ █▀█ █▀█
+█░▀░█ ██▄ █▄▄ █▄▄ █▄█ █▀▄
+
+|| SOFTWARE DEVELOPER ||
+</pre>
     <?php foreach (SECTION_NOWINDOWS as $lang => $output) { ?>
         <p lang="<?php echo $lang; ?>"><?php echo $output; ?></p>
     <?php } ?>
